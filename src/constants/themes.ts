5a3039ab9b35c4a1f992024d86ba84ac3364a2ea
bidetaggle <bidetaggle@gmail.com>
2021-07-29 18:59:23 +0200

    ⚙️ add material-ui configurations (https://material-ui.com/guides/typescript/)

--- src/constants/themes.ts
+++ src/constants/themes.ts
@@ -1,15 +1,23 @@
 import { PaletteType } from '@material-ui/core';
 
+const main = {
+  typography: {
+    fontFamily: 'monospace'
+  }
+}
+
 const light = {
+  ...main,
   palette: {
     type: 'light' as PaletteType
-  }
+  },
 };
 
 const dark = {
+  ...main,
   palette: {
     type: 'dark' as PaletteType
-  }
+  },
 };
 
 export {light, dark};
