3bbe43d35beeaad071e3afd4229012d7d104fae0
bidetaggle <bidetaggle@gmail.com>
2021-08-01 00:21:40 +0200

    🆕 set theme according to system theme mode

--- src/components/Header/ThemeButton.tsx
+++ src/components/Header/ThemeButton.tsx
@@ -1,12 +1,17 @@
-import {useContext} from 'react';
+import {useContext, useEffect} from 'react';
 import {IconButtonS} from '../../style/components/material-ui';
 import Brightness2Icon from '@material-ui/icons/Brightness2';
 import WbSunnyIcon from '@material-ui/icons/WbSunny';
 import ctx from '../../constants/ctx';
 
+const mq = window.matchMedia('(prefers-color-scheme: dark)');
+
 function ThemeButton() {
   const {theme, setTheme} = useContext(ctx);
   const icon = theme ? <Brightness2Icon /> : <WbSunnyIcon />
+  
+  // eslint-disable-next-line
+  useEffect(() => {setTheme(!mq.matches)}, []);
 
   return(
     <IconButtonS
