00cce783f14f2f6904fd0bb463a3c87129509219
bidetaggle <bidetaggle@gmail.com>
2021-08-01 14:32:38 +0200

    ✨ hide App bar on scroll down

--- src/components/Header/AppBar.tsx
+++ src/components/Header/AppBar.tsx
@@ -3,26 +3,41 @@ import LoginButton from './LoginButton';
 import ThemeButton from './ThemeButton';
 import {AppBarS} from '../../style/components/material-ui';
 import img from '../../constants/img';
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import Slide from "@material-ui/core/Slide";
+
+function HideOnScroll({children}: {children: React.ReactElement}) {
+  const trigger = useScrollTrigger();
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 
 function Header() {
-  return (
-    <AppBarS position="static">
-      <Toolbar>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-        >
-          <img src={img.logo} alt="arweave logo" height="50" />
-          <div>
-            <ThemeButton />
-            <LoginButton />
-          </div>
-        </Grid>
-      </Toolbar>
-    </AppBarS>
-  );
+  return (<>
+    <HideOnScroll>
+      <AppBarS>
+        <Toolbar>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+          >
+            <img src={img.logo} alt="arweave logo" height="50" />
+            <div>
+              <ThemeButton />
+              <LoginButton />
+            </div>
+          </Grid>
+        </Toolbar>
+      </AppBarS>
+    </HideOnScroll>
+    <Toolbar />
+  </>);
 }
 
 export default Header;
