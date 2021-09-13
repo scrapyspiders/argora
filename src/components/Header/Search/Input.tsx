2af916dc9de35a275c164d73dd0866c402bf503f
bidetaggle <bidetaggle@gmail.com>
2021-09-13 22:23:37 +0200

    🆕 add wallet address profile search

--- src/components/Header/Search/Input.tsx
+++ src/components/Header/Search/Input.tsx
@@ -1,9 +1,9 @@
 import { useEffect, useRef } from "react";
 import { InfoS, InputLeftS, InputS } from "../../../style/components/Header";
 
-const placeHolder = "search / create a planet 🪐";
+const placeHolder = "search / create a planet 🪐 or search a wallet";
 
-function Input({showInfo, onChange, value}: {showInfo: number | null, onChange: (e: React.FormEvent<HTMLInputElement>) => void, value: string}){
+function Input({showInfo, onChange, value}: {showInfo: string | number | null, onChange: (e: React.FormEvent<HTMLInputElement>) => void, value: string}){
   const didMount = useRef(false);
   const inputCropped = useRef<HTMLInputElement>(null);
   const inputOriginal = useRef<HTMLInputElement>(null);
@@ -21,8 +21,13 @@ function Input({showInfo, onChange, value}: {showInfo: number | null, onChange: 
 
   return(showInfo !== null
     ? <InputLeftS>
-        <InfoS>
-          {showInfo > 0 ? `${showInfo} weeve${showInfo > 1 ? 's' : ''}` : "NEW"}
+        
+        <InfoS>{
+          Number.isInteger(showInfo) 
+          ? showInfo > 0
+            ? `${showInfo} weeve${showInfo > 1 ? 's' : ''}`
+            : "NEW"
+          : showInfo}
         </InfoS>
         <InputS
           list="search"
--- src/components/Header/Search/index.tsx
+++ src/components/Header/Search/index.tsx
@@ -13,9 +13,10 @@ let typingTimeout: any = null;
 function Search({className}: {className?: string}) {
   const history = useHistory();
   const {pathBase, planet} = useParams<PathParams>();
-  const [planetTyped, setPlanetTyped] = useState<string>("");
+  const [value, setValue] = useState<string>("");
   const [showPostsN, setShowPostsN] = useState<number | null>(null);
   const [planetsList, setPlanetsList] = useState<[T_planet]>();
+  const [isProfile, setIsProfile] = useState(false);
 
   const postsNumber = async (planetName: string) => {
     if(planetName.length < 1)
@@ -28,7 +29,10 @@ function Search({className}: {className?: string}) {
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     const val = e.currentTarget.value;
-    setPlanetTyped(val);
+    setValue(val);
+
+    const isAddr = /^[a-zA-Z0-9\-_]{43}$/.test(val);
+    isAddr ? setIsProfile(true) : setIsProfile(false);
     
     if(typingTimeout) clearTimeout(typingTimeout);
     typingTimeout = setTimeout(() => postsNumber(val), 300);
@@ -36,15 +40,15 @@ function Search({className}: {className?: string}) {
 
   const handleSubmit = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    if(planetTyped.length > 0){
-      history.push(`/${pathBase}/${planetTyped}`);
+    if(value.length > 0){
+      history.push(/^[a-zA-Z0-9\-_]{43}$/.test(value) ? `/${pathBase}/profile/${value}` : `/${pathBase}/${value}`);
     }
   }
 
   useEffect(() => {
     console.log("useEffect search");
     setShowPostsN(null);
-    setPlanetTyped(planet ? planet : "");
+    setValue(planet ? planet : "");
     const planetsListLocalStorage = localStorage.getItem('planets');
     setPlanetsList(planetsListLocalStorage ? JSON.parse(planetsListLocalStorage) : []);
   }, [planet]);
@@ -52,9 +56,9 @@ function Search({className}: {className?: string}) {
   return(
     <FormS onSubmit={handleSubmit} className={className}>
       <Input
-        showInfo={showPostsN}
+        showInfo={isProfile ? "profile" : showPostsN}
         onChange={handleChange}
-        value={planetTyped}
+        value={value}
       />
       <datalist id="search">
         {planetsList?.map((p, i) => <option value={p} key={i} />)}
